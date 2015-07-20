1829871ca1a38abbd34204a24ac32403f5200a62
toneylee <ducsangpro.93@gmail.com>
2015-07-20 22:06:23 +0700

    Create business layer

--- Technical/BusinessLayer/BUSCustomer.cs
+++ Technical/BusinessLayer/BUSCustomer.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccessLayer;
 
 namespace BusinessLayer
 {
     public class BUSCustomer
     {
-        
+        public DAOCustomer daoCustomer;
+        public BUSCustomer()
+        {
+            this.daoCustomer = new DAOCustomer();
+        }
+
+        public void CreateCustomer(CUSTOMER customer)
+        {
+            this.daoCustomer.CreateCustomer(customer);
+        }
+
+        public void UpdateCustomer(CUSTOMER customer)
+        {
+            this.daoCustomer.UpdateCustomer(customer);
+        }
+
+        public void DeleteCustomer(CUSTOMER customer)
+        {
+            this.daoCustomer.DeleteCustomer(customer);
+        }
+
+        public void DeleteCustomer(string customerID)
+        {
+            this.daoCustomer.DeleteCustomer(customerID);
+        }
     }
 }
