d788aff2c0e6c9f9f16fe2af076738417d1c2e17
toneylee <ducsangpro.93@gmail.com>
2015-07-20 21:55:20 +0700

    Create project + database

---
+++ Technical/DataAccessLayer/DAOCustomer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DAOCustomer
+    {
+        public void CreateCustomer(CUSTOMER customer)
+        {
+            DataContextInitializer.DbContext.CUSTOMERs.InsertOnSubmit(customer);
+            DataContextInitializer.DbContext.SubmitChanges();
+        }
+
+        public void UpdateCustomer(CUSTOMER customer)
+        {
+            IQueryable<CUSTOMER> _customerList = DataContextInitializer.DbContext.CUSTOMERs.Where(cus=>cus.CustomerID == customer.CustomerID);
+            foreach (CUSTOMER _customer in _customerList)
+            {
+                _customer.CustomerID = customer.CustomerID;
+                _customer.Address = customer.Address;
+                _customer.CustomerName = customer.CustomerName;
+                _customer.DateOfBirth = customer.DateOfBirth;
+                _customer.Gender = customer.Gender;
+                _customer.IDNumber = customer.IDNumber;
+                _customer.PhoneNumber = customer.PhoneNumber;
+            }
+            DataContextInitializer.DbContext.SubmitChanges();
+        }
+
+        public void DeleteCustomer(CUSTOMER customer)
+        {
+            DataContextInitializer.DbContext.CUSTOMERs.DeleteOnSubmit(customer);
+            DataContextInitializer.DbContext.SubmitChanges();
+        }
+
+        public void DeleteCustomer(string customerID)
+        {
+            IQueryable<CUSTOMER> _customerList = DataContextInitializer.DbContext.CUSTOMERs.Where(cus => cus.CustomerID == customerID);
+            foreach (CUSTOMER _customer in _customerList)
+            {
+                DataContextInitializer.DbContext.CUSTOMERs.DeleteOnSubmit(_customer);
+            }
+            DataContextInitializer.DbContext.SubmitChanges();
+        }
+    }
+}
